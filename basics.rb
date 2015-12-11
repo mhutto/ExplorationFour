c4e7a9089bfcd18343b040f4664c12f97313c4ca
Morgan <mmhkwc@mail.missouri.edu>
2015-12-11 13:23:02 -0600

    Adding Work

---
+++ basics.rb
@@ -0,0 +1,51 @@
+#Lesson 1.1
+
+my_num = 25     # Add your code here!
+
+my_boolean = true    # And here!
+
+my_string = "Ruby"    # Also here.
+
+#Lesson 1.2
+
+my_num = 100
+
+
+#Lesson 1.3
+
+my_num = 9%3
+
+#Lesson 1.4
+
+puts "What's up?"
+print "Oxnard Montalvo"
+print "testing this"
+
+#Lesson 2
+"Morgan Hutton".length
+
+=begin
+I'm a long comment
+I take up two lines
+=end
+
+name = "Morgan"
+
+#Review
+my_name = "Morgan"
+my_age = 22
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209/17
+
+name = "Morgan".downcase.reverse.upcase
+
+#single line
+
+=begin
+double
+line
+=end
+
+
---
+++ formprogram.rb
@@ -0,0 +1,47 @@
+print "What's your first name?"
+
+
+first_name = gets.chomp
+
+
+print "What's your first name?"
+
+first_name = gets.chomp
+
+print "What's your last name?"
+
+last_name = gets.chomp
+
+print "Where are you from?"
+
+ city = gets.chomp
+ 
+ print "What state?"
+ 
+ state.upcase = gets.chomp
+
+
+
+print "What's your first name?"
+
+first_name = gets.chomp
+first_name2 = first_name.capitalize
+first_name.capitalize!
+
+print "What's your last name?"
+
+last_name = gets.chomp
+last_name2 = last_name.capitalize
+last_name.capitalize!
+
+print "Where are you from?"
+
+ city = gets.chomp
+ city2 = city.capitalize
+city.capitalize!
+ 
+ print "What state?"
+ 
+ state = gets.chomp
+ state2 = state.upcase
+ state.upcase!
